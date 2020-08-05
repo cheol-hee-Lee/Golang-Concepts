a44629d615b7baca847f2cf58d3e30dbd827d5d0
cheolheeLee <201802141@o.cnu.ac.kr>
2020-08-05 14:56:20 +0900

    Add .go files

--- concepts/main.go
+++ concepts/main.go
@@ -1,38 +1,21 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
-type Article struct {
-	Title   string `json:"title"`
-	Desc    string `json:"desc"`
-	Content string `json:"content"`
-}
-
-type Articles []Article
-
-func allArticles(w http.ResponseWriter, r *http.Request) {
-	articles := Articles{
-		Article{Title: "test title", Desc: "test desc", Content: "test content"},
-	}
-	fmt.Println("Endpoint Hit: All Articles Endpoint")
-	json.NewEncoder(w).Encode(articles)
-}
-
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hompage Endpoint Hit")
-}
-
-func handleRequest() {
-	http.HandleFunc("/", homePage)
-	http.HandleFunc("/articles", allArticles)
-
-	log.Fatal(http.ListenAndServe(":8881", nil))
-}
 func main() {
-	handleRequest()
+	res, err := http.Get("http://www.naver.com/")
+	if err != nil {
+		log.Fatal(err)
+	}
+	page, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s", page)
 }
